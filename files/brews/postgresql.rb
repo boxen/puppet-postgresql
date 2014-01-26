22c486bbf2517bd735de43b7bb768aeb51ca2c14
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-01-26 18:37:41 -0200

    Upgrade to 9.3.2

--- files/brews/postgresql.rb
+++ files/brews/postgresql.rb
@@ -2,20 +2,24 @@ require 'formula'
 
 class Postgresql < Formula
   homepage 'http://www.postgresql.org/'
-  url 'http://ftp.postgresql.org/pub/source/v9.3.1/postgresql-9.3.1.tar.bz2'
-  sha256 '8ea4a7a92a6f5a79359b02e683ace335c5eb45dffe7f8a681a9ce82470a8a0b8'
-  version '9.3.1-boxen'
+  url 'http://ftp.postgresql.org/pub/source/v9.3.2/postgresql-9.3.2.tar.bz2'
+  sha256 '700da51a71857e092f6af1c85fcd86b46d7d5cd2f2ba343cafb1f206c20232d7'
+  version '9.3.2-boxen'
+
+  option '32-bit'
+  option 'no-perl', 'Build without Perl support'
+  option 'no-tcl', 'Build without Tcl support'
+  option 'enable-dtrace', 'Build with DTrace support'
 
   depends_on 'readline'
-  depends_on 'ossp-uuid'
+  depends_on 'ossp-uuid' => :recommended
 
-  def options
-    [
-      ['--32-bit', 'Build 32-bit only.'],
-      ['--no-python', 'Build without Python support.'],
-      ['--no-perl', 'Build without Perl support.'],
-      ['--enable-dtrace', 'Build with DTrace support.']
-    ]
+  conflicts_with 'postgres-xc',
+    :because => 'postgresql and postgres-xc install the same binaries.'
+
+  fails_with :clang do
+    build 211
+    cause 'Miscompilation resulting in segfault on queries'
   end
 
   # Fix uuid-ossp build issues: http://archives.postgresql.org/pgsql-general/2012-07/msg00654.php
@@ -23,48 +27,40 @@ class Postgresql < Formula
     DATA
   end
 
-  skip_clean :all
-
-  fails_with :clang do
-    build 211
-    cause 'Miscompilation resulting in segfault on queries'
-  end
-
   def install
-    ENV.libxml2 if MacOS.snow_leopard?
+    ENV.libxml2 if MacOS.version >= :snow_leopard
 
-    args = [
-      "--disable-debug",
-      "--prefix=#{prefix}",
-      "--datadir=#{share}/#{name}",
-      "--docdir=#{doc}",
-      "--enable-thread-safety",
-      "--with-bonjour",
-      "--with-gssapi",
-      "--with-krb5",
-      "--with-openssl",
-      "--with-libxml",
-      "--with-libxslt",
-      "--with-libedit"
+    args = %W[
+      --disable-debug
+      --prefix=#{prefix}
+      --datadir=#{share}/#{name}
+      --docdir=#{doc}
+      --enable-thread-safety
+      --with-bonjour
+      --with-gssapi
+      --with-krb5
+      --with-ldap
+      --with-openssl
+      --with-pam
+      --with-libxml
+      --with-libxslt
     ]
 
-    args << "--with-ossp-uuid" unless ARGV.include? '--no-ossp-uuid'
-    args << "--with-python" unless ARGV.include? '--no-python'
-    args << "--with-perl" unless ARGV.include? '--no-perl'
-    args << "--with-tcl" unless ARGV.include? '--no-tcl'
-    args << "--enable-dtrace" if ARGV.include? '--enable-dtrace'
+    args << "--with-ossp-uuid" if build.with? 'ossp-uuid'
+    args << "--with-perl" unless build.include? 'no-perl'
+    args << "--with-tcl" unless build.include? 'no-tcl'
+    args << "--enable-dtrace" if build.include? 'enable-dtrace'
 
-    ENV.append 'CFLAGS', `uuid-config --cflags`.strip
-    ENV.append 'LDFLAGS', `uuid-config --ldflags`.strip
-    ENV.append 'LIBS', `uuid-config --libs`.strip
-
-    if ARGV.build_32_bit?
-      ENV.append 'CFLAGS', '-arch i386'
-      ENV.append 'LDFLAGS', '-arch i386'
+    if build.with? 'ossp-uuid'
+      ENV.append 'CFLAGS', `uuid-config --cflags`.strip
+      ENV.append 'LDFLAGS', `uuid-config --ldflags`.strip
+      ENV.append 'LIBS', `uuid-config --libs`.strip
     end
 
-    # Fails on Core Duo with O4 and O3
-    ENV.O2 if Hardware.intel_family == :core
+    if build.build_32_bit?
+      ENV.append 'CFLAGS', "-arch #{MacOS.preferred_arch}"
+      ENV.append 'LDFLAGS', "-arch #{MacOS.preferred_arch}"
+    end
 
     system "./configure", *args
     system "make install-world"
@@ -72,8 +68,8 @@ class Postgresql < Formula
 end
 
 __END__
---- a/contrib/uuid-ossp/uuid-ossp.c 2012-07-30 18:34:53.000000000 -0700
-+++ b/contrib/uuid-ossp/uuid-ossp.c 2012-07-30 18:35:03.000000000 -0700
+--- a/contrib/uuid-ossp/uuid-ossp.c        2012-07-30 18:34:53.000000000 -0700
++++ b/contrib/uuid-ossp/uuid-ossp.c        2012-07-30 18:35:03.000000000 -0700
 @@ -9,6 +9,8 @@
   *-------------------------------------------------------------------------
   */
--- manifests/params.pp
+++ manifests/params.pp
@@ -11,7 +11,7 @@ class postgresql::params {
       $port       = 15432
 
       $package    = 'boxen/brews/postgresql'
-      $version    = '9.3.1-boxen'
+      $version    = '9.3.2-boxen'
 
       $service    = 'dev.postgresql'
 
