3c96c4e1f4816349f2359a9d21b2c6826a9f6739
Danny Fallon <danny@intercom.io>
2014-05-19 12:08:04 +0100

    Add password support during role creation

--- manifests/role.pp
+++ manifests/role.pp
@@ -3,8 +3,10 @@
 # Usage:
 #
 #     postgresql::role { 'rolename': }
-define postgres::role (
-  $password_hash    = false,
+define postgresql::role (
+  $ensure           = present,
+  $username         = $name,
+  $password         = undef,
   $createdb         = false,
   $createrole       = false,
   $login            = true,
@@ -12,17 +14,28 @@ define postgres::role (
   $superuser        = false,
   $replication      = false,
   $connection_limit = '-1',
-  $username         = $name
 ) {
   require postgresql
-  notify { "Creating user ${$username}": }
+
+  $createdb_sql    = $createdb    ? { true => 'CREATEDB',    default => 'NOCREATEDB' }
+  $createrole_sql  = $createrole  ? { true => 'CREATEROLE',  default => 'NOCREATEROLE' }
+  $login_sql       = $login       ? { true => 'LOGIN',       default => 'NOLOGIN' }
+  $inherit_sql     = $inherit     ? { true => 'INHERIT',     default => 'NOINHERIT' }
+  $superuser_sql   = $superuser   ? { true => 'SUPERUSER',   default => 'NOSUPERUSER' }
+  $replication_sql = $replication ? { true => 'REPLICATION', default => '' }
+
+  if ($password != undef) {
+    $password_sql = "ENCRYPTED PASSWORD '${password}'"
+  } else {
+    $password_sql = ''
+  }
 
   exec { "postgresql-createuser-${name}":
     command => join([
-    'createuser',
-    "-p${postgresql::port}",
-    $username
+      'psql',
+      "-p${postgresql::port}",
+      "-c \"CREATE ROLE \"${username}\" ${password_sql} ${login_sql} ${createrole_sql} ${createdb_sql} ${superuser_sql} ${replication_sql} CONNECTION LIMIT ${connection_limit}\""
     ], ' '),
-    unless  => "psql -aA -p${postgresql::port} -t -l | tail +2 | grep -w '${username}'"
+    unless  => "psql -aA -p${postgresql::port} -t -c \"SELECT rolname FROM pg_roles WHERE rolname='${username}'\" | tail +2 | grep ${username}"
   }
 }
