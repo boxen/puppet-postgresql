6b7bac3028e3f9094a7ee7a3763847ce5b57a38d
Danny Fallon <danny@intercom.io>
2014-10-04 17:59:05 +0100

    Add tests for specifying a DB owner

--- spec/defines/postgresql_db_spec.rb
+++ spec/defines/postgresql_db_spec.rb
@@ -6,10 +6,25 @@ describe "postgresql::db" do
 
   it do
     should include_class("postgresql")
+  end
 
-    should contain_exec("postgresql-db-#{title}").with({
-      :command => "createdb -p15432 -E UTF-8 -O testuser #{title}",
-      :unless  => "psql -aA -p15432 -t -l | cut -d \\| -f 1 | grep -w '#{title}'"
-    })
+  context "without a specified db owner" do
+    it do
+      should contain_exec("postgresql-db-#{title}").with({
+        :command => "createdb -p15432 -E UTF-8 -O testuser #{title}",
+        :unless  => "psql -aA -p15432 -t -l | cut -d \\| -f 1 | grep -w '#{title}'"
+      })
+    end
+  end
+
+  context "with a specified db owner" do
+    let(:params) { { :owner => 'foo' } }
+
+    it do
+      should contain_exec("postgresql-db-#{title}").with({
+        :command => "createdb -p15432 -E UTF-8 -O foo #{title}",
+        :unless  => "psql -aA -p15432 -t -l | cut -d \\| -f 1 | grep -w '#{title}'"
+      })
+    end
   end
 end
